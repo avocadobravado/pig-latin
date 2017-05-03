9d71e8132c1f4b850c2a8bde307a4a54006bf0b0
Daniela Trulls and Rose Sponseller <daniela@>
2017-05-03 15:55:49 -0700

    Results will display correctly if a single word beginning with three consonants is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,11 @@ $(function(){
     var userInput = $("#userInput").val();
     var output = [];
     var vowels = ["a", "e", "i", "o", "u"];
-    var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
+    var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "y" , "w", "x", "z"];
     var y = "y";
     var qu = ["q", "u"];
+    var secondConsonant;
+    var firstConsonant;
 
 //Converts to lowercase
     var convertLowerCase = function(){
@@ -18,20 +20,35 @@ $(function(){
 //Adds way to the end of a string (begginig with VOWEL)
     for(var index = 0; index < vowels.length; index +=1){
       if (userInput[0] === vowels[index]) {
-        userInput += "way";
+      var userInput0 = userInput + "way";
+      $("#output").text(userInput0);
       }
     }
 // //Adds ay to the end of a string (begginig with CONSONANT)
-for(var index = 0; index < consonants.length; index +=1){
-  if (userInput[0] === consonants[index]) {
-    var firstConsonant = userInput.charAt(0).slice(-1);
-    // console.log(firstConsonant);
-    userInput = userInput.slice(1);
-    //console.log(userInput);
-    userInput = userInput + firstConsonant + "ay";
-  //  console.log(userInput);
-  }
-}
+    for(var index = 0; index < consonants.length; index +=1){
+      if (userInput[0] === consonants[index]) {
+        firstConsonant = userInput.charAt(0).slice(-1);
+        userInput1 = userInput.slice(1);
+        userInput1 = userInput1 + firstConsonant + "ay";
+       $("#output").text(userInput1);
+      } else if (userInput[1] === consonants[index]){
+       secondConsonant = userInput.charAt(1).slice(-1);
+       var userInput2 = userInput.replace(userInput[0], "").replace(userInput[1], "") + firstConsonant + secondConsonant + "ay";
+       $("#output").text(userInput2);
+     } else if (userInput[2] === consonants[index]){
+       firstConsonant = userInput.charAt(0).slice(-1);
+       secondConsonant = userInput.charAt(1).slice(-1);
+       var thirdConsonant = userInput.charAt(2).slice(-1);
+       console.log(thirdConsonant);
+       var userInput3 = userInput.replace(userInput[0], "").replace(userInput[1], "").replace(userInput[2], "") + firstConsonant + secondConsonant + thirdConsonant + "ay";
+       console.log(userInput3);
+     }
+     $("#output").text(userInput3);
+    }
+
+
+//TRY TO CONVERT USERINPUT INTO AN ARRAY
+
 
     // for(var index = 0; index < consonants.length; index +=1){
     //   if (userInput[0] === consonants[index] || userInput[1] === consonants[index]) {
@@ -58,7 +75,7 @@ for(var index = 0; index < consonants.length; index +=1){
     // }
 
 
-    $("#output").text(userInput);
+
   });
 });
 
